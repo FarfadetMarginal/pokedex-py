4b3d83d4dc2e8d417cabf0c5a2cc2ab649467540
FarfadetMarginal <bapt935@gmail.com>
2026-02-02 09:35:03 +0100

    fonction afficher infos done

--- exopokedex.py
+++ exopokedex.py
@@ -15,8 +15,22 @@ class Pokemon:
         self.spdef = defensespeciale
         self.speed = vitesse
 
+    def afficher_infos(self):
+        print(f"Nom : {self.name}")
+        print(f"Pokemon numéro  {self.number}")
+        print(f"Type : {self.type}")
+        print(f"Il mesure {self.height} et pèse {self.weight}")
+        print(f"Statistiques : ")
+        print(f"PV = {self.hp}")
+        print(f"Attaque = {self.attack}")
+        print(f"Defense = {self.defense}")
+        print(f"Attaque spéciale = {self.spatt}")
+        print(f"Defense spéciale = {self.spdef}")
+        print(f"Vitesse = {self.speed}")
 
 
+bublizarre = Pokemon("Bulbizarre", 1, ["plante", "poison"], 0.7, 6.9, 45, 49, 49, 65, 65, 45)
+print(Pokemon.self.name)
 
 
 
